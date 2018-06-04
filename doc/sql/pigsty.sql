ddb91ed434361f4ec34a9059a40c3bbc9777538d
Zhangsan <1665296716@qq.com>
2018-06-04 10:09:40 +0800

    java project

--- doc/sql/pigsty.sql
+++ doc/sql/pigsty.sql
@@ -1,12 +1,94 @@
+/*
+SQLyog Ultimate v11.25 (64 bit)
+MySQL - 5.7.22 : Database - pigsty
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`pigsty` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
 USE `pigsty`;
 
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `id` int(10) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `url` varchar(200) DEFAULT NULL,
+  `permission` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `parent_id` int(10) DEFAULT NULL,
+  `parent_ids` varchar(50) DEFAULT NULL,
+  `status` enum('PENDING','ACTIVE','INACTIVE','DELETED') DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_permission` */
+
+LOCK TABLES `sys_permission` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `role` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `level` int(10) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `status` enum('PENDING','ACTIVE','INACTIVE','DELETED') DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role` */
+
+LOCK TABLES `sys_role` WRITE;
+
+insert  into `sys_role`(`id`,`role`,`name`,`level`,`description`,`status`,`create_date`,`update_date`) values (1,NULL,'ADMIN',1,'超级管理员','ACTIVE','2018-05-27 17:47:25',NULL),(2,NULL,'USER',2,'普通用户','ACTIVE','2018-05-27 17:47:45',NULL);
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_role_permission` */
+
+DROP TABLE IF EXISTS `sys_role_permission`;
+
+CREATE TABLE `sys_role_permission` (
+  `role_id` int(10) NOT NULL,
+  `permission_id` int(10) NOT NULL,
+  PRIMARY KEY (`role_id`,`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role_permission` */
+
+LOCK TABLES `sys_role_permission` WRITE;
+
+UNLOCK TABLES;
+
 /*Table structure for table `sys_user` */
+
 DROP TABLE IF EXISTS `sys_user`;
+
 CREATE TABLE `sys_user` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
+  `salt` varchar(100) DEFAULT NULL,
   `nickname` varchar(20) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -14,4 +96,35 @@ CREATE TABLE `sys_user` (
   `create_date` datetime DEFAULT NULL,
   `update_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user` */
+
+LOCK TABLES `sys_user` WRITE;
+
+insert  into `sys_user`(`id`,`username`,`password`,`salt`,`nickname`,`phone`,`email`,`status`,`create_date`,`update_date`) values (17,'admin','d3f0253ef8254c887e2750f6cfabcdf8','4c0142940908173778f3fa7439ecff53',NULL,NULL,NULL,'ACTIVE','2018-06-03 17:52:38',NULL);
+
+UNLOCK TABLES;
+
+/*Table structure for table `sys_user_role` */
+
+DROP TABLE IF EXISTS `sys_user_role`;
+
+CREATE TABLE `sys_user_role` (
+  `user_id` int(10) NOT NULL,
+  `role_id` int(10) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user_role` */
+
+LOCK TABLES `sys_user_role` WRITE;
+
+insert  into `sys_user_role`(`user_id`,`role_id`) values (17,1);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
