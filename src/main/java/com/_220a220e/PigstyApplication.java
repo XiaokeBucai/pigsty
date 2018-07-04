ec0098492e3e9a58da8f53ecfa63ef2beca7568b
ZhangYong <zhangyong3@ctsi.com.cn>
2018-07-04 15:47:05 +0800

    Redis

--- src/main/java/com/_220a220e/PigstyApplication.java
+++ src/main/java/com/_220a220e/PigstyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author Garrosh
@@ -12,6 +13,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 @MapperScan("com._220a220e.mapper")
+@EnableCaching
 public class PigstyApplication extends SpringBootServletInitializer {
 
 	@Override
